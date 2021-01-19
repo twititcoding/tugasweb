fb92866875821c82fd2a2be0dc1d20472ca86105
twititcoding <76032103+twititcoding@users.noreply.github.com>
2021-01-19 20:52:46 +0700

    Add files via upload
    portfolio tugas pemrograman berbasis web

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// jequery
+$(document).ready(function () {
+    $("#flip").click(function () {
+        $("#panel").toggle("slow");
+    });
+    $("#flip1").click(function () {
+        $("#panel1").toggle("slow");
+    });
+    $("#flip2").click(function () {
+        $("#panel2").toggle("slow");
+    });
+    $("#flip3").click(function () {
+        $("#panel3").toggle("slow");
+    });
+
+});
